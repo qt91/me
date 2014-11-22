ca600d0b1ffd10a67ab9a7114429fb46be314f7b
Quang Thịnh <quangthinh.nguyen91@gmail.com>
2014-11-22 21:12:44 +0700

    Update

--- codeigniter/mycore/controller.php
+++ codeigniter/mycore/controller.php
@@ -1,9 +1,13 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class profile extends ALTA_Controller{
+class {ctName} extends ALTA_Controller{
     public function __construct() {
         parent::__construct();
-        $this->load->model('profile/mdl_profile');
+    }
+
+    public function index(){
+
+    	$this->load->view('index',$this->data);
     }
 
 }
