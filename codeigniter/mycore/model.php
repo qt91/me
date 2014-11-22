f71d0d2255ee7e76a8625635f80f0a82e21abd8c
Quang Thịnh <quangthinh.nguyen91@gmail.com>
2014-11-22 20:18:04 +0700

    Update

--- codeigniter/mycore/model.php
+++ codeigniter/mycore/model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class {ClName} extends ALTA_Model {
+class {mdlName} extends ALTA_Model {
 
     public function __construct() {
         parent::__construct('{tbl_id}','{tbl_name}');
